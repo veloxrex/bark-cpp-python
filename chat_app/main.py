dc91ac7b8af1767b450bfa549d191a579f9dd420
VN20K20054 <tri.tranminh@zoi.tech>
2024-10-25 14:12:08 +0700

    chat_app

--- chat_app/main.py
+++ chat_app/main.py
@@ -1,7 +1,7 @@
 # main.py
 from PyQt6 import QtWidgets
 import sys
-from chat_app.chat_app import ChatApp
+from components.chat_app import ChatApp
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
